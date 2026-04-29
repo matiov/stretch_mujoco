39c7d08ce24109a3b276d4031f3d606d11e060af
Matteo Iovino <matteo.iovino93@gmail.com>
2026-04-29 16:28:31 +0200

    Revise logic

--- examples/auto_grasp_test.py
+++ examples/auto_grasp_test.py
@@ -4,10 +4,10 @@ from stretch_mujoco.enums.actuators import Actuators
 
 # --- CONFIGURATION ---
 # Adjust these values as needed for your scene/object
-ARM_ALIGN_POS = 0.15  # meters, arm extension to align with object
-LIFT_ALIGN_POS = 0.40  # meters, lift height to align with object
-GRIPPER_OPEN_POS = 0.3  # open gripper (positive value)
-GRIPPER_CLOSE_POS = -0.15  # close gripper (negative value)
+ARM_ALIGN_POS = 0.20  # meters, arm extension to align with object
+LIFT_ALIGN_POS = 0.42  # meters, lift height to align with object
+GRIPPER_OPEN_POS = 0.25  # open gripper (positive value)
+GRIPPER_CLOSE_POS = 0.01  # close gripper (negative value)
 LIFT_LIFTED_POS = 0.60  # lift after grasp
 
 
@@ -46,17 +46,18 @@ def main():
     print("[STEP 5] Done. Check if object is held.")
     input("Press Enter to continue to the next step...")
 
-    # 6. Releasing object
-    print("[STEP 6] Releasing object. The object should fall now...")
+    # 6. Stowing arm
+    print("[STEP 6] Stowing the arm")
+    sim.stow()
+    input("Press Enter to continue to the next step...")
+
+    # 7. Releasing object
+    print("[STEP 7] Releasing object. The object should fall now...")
     sim.move_to(Actuators.gripper, GRIPPER_OPEN_POS)
     sim.wait_until_at_setpoint(Actuators.gripper)
 
-    # 6. Releasing object
-    # print("[STEP 6] Stowing the arm")
-    # sim.stow()
+    # Shutting Down
     input("Press Enter to continue to the next step...")
-    
-
     print("[END] We're done here! Bye Bye!")
     sim.stop()
 
